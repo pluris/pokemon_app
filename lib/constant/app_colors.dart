fbe335b0725e198b2f8aac3d9606e520cc97db80
pluris <zighart8456@naver.com>
2023-06-30 23:54:17 +0900

    Add constants(color,images)

---
+++ lib/constant/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const blue = Color(0xff141a31);
+const gray = Color(0xBDEFEFEF);
+const white = Color(0xFFFFFFFF);
+const blueShade100 = Color(0xFF89C5E0);
+const yellow = Color(0xFFFFCB05);
