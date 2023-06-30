fe576edab96f978b1c23fac99f6f5d7a665237b6
pluris <zighart8456@naver.com>
2023-07-01 00:44:45 +0900

    Remove initialize delay in main

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,11 @@ import 'package:pokemon_app/app.dart';
 import 'package:pokemon_app/presentation/bloc/details/details_bloc.dart';
 import 'package:pokemon_app/presentation/bloc/pokemon/pokemon_bloc.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   runApp(MultiBlocProvider(providers: [
     BlocProvider(create: (context) => PokemonBloc()),
     BlocProvider(create: (context) => DetailsBloc()),
   ], child: const MyApp()));
-}
-
-Future initialization(BuildContext? context) async {
-  await Future.delayed(const Duration(seconds: 2));
 }
